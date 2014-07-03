f9715ddbe45f0a537649561c1583824b5a4ed251
Ajay Kesar <ajaykes@hotmail.de>
2014-07-03 18:38:20 +0200

    ExplorationManager fixed

--- de.hpi.sam.robotino/src/RobotSystem/Implementation/ExplorationManager.java
+++ de.hpi.sam.robotino/src/RobotSystem/Implementation/ExplorationManager.java
@@ -6,13 +6,10 @@ import java.util.List;
 
 import Datatypes.Added.RoomPoint;
 import Datatypes.Added.Route;
+import RobotSystem.Implementation.RouteFinder;
 import RobotSystem.Interfaces.New.IExplorationManager;
-import RobotSystem.Interfaces.New.IRouteFinder;
-import de.cpslab.robotino.environment.Position;
 import de.hpi.sam.warehouse.WarehouseRobot;
 import de.hpi.sam.warehouse.environment.IWarehouseEnvironment;
-import de.hpi.sam.warehouse.order.Order;
-import de.hpi.sam.warehouse.stock.Stockroom;
 import de.hpi.sam.warehouse.stock.StockroomID;
 import de.hpi.sam.warehouse.stock.WarehouseRepresentation;
 
@@ -22,19 +19,22 @@ public class ExplorationManager implements IExplorationManager {
 	private boolean done;
 	private Route currentRoute;
 	private WarehouseRepresentation representation;
-	private RouteFinder rf = new RouteFinder();
+	private RouteFinder rf;
 	private DriveManager dm;
 	private WarehouseRobot robot;
 	
 	public ExplorationManager(WarehouseRobot robot) {
 		this.robot = robot;
 		dm = new DriveManager(robot);
+		representation = new WarehouseRepresentation();
+		rf = new RouteFinder(robot, representation);
+		
 	}
 	
 	@Override
 	public Date calculateExplorationTime(StockroomID room) {
-		// TODO assumption: distance in mm
-		int distance = rf.getDistance(rf.calculateExplorationRoute(getPosition(), room));
+		// TODO assumption: distance in mm and cast from double to int
+		int distance = (int) rf.getDistance(rf.calculateExplorationRoute(rf.getPosition(), room));
 		
 		// time in milli seconds
 		int time = distance/IWarehouseEnvironment.SAFETY_SPEED * 1000;
@@ -44,7 +44,7 @@ public class ExplorationManager implements IExplorationManager {
 	}
 	@Override
 	public void explorationStart(StockroomID room) {
-		Route route = rf.calculateExplorationRoute(getPosition(), room);
+		Route route = rf.calculateExplorationRoute(rf.getPosition(), room);
 		List<RoomPoint> rp = route.getRoomPoints();
 		
 		for (RoomPoint r : rp) {
