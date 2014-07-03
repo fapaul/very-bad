f8083c5bb48b35e70d5bba89f4bfa1e569062ac5
Ajay Kesar <ajaykes@hotmail.de>
2014-07-04 01:52:22 +0200

    meine Simulation, meine RobotBehaviour, meine CartSource

--- de.hpi.sam.robotino/src/Datatypes/Added/CartSource.java
+++ de.hpi.sam.robotino/src/Datatypes/Added/CartSource.java
@@ -7,22 +7,26 @@ import de.hpi.sam.warehouse.WarehouseRobot;
 import de.hpi.sam.warehouse.stock.Cart;
 import de.hpi.sam.warehouse.stock.CartArea;
 import de.hpi.sam.warehouse.stock.CartPosition;
+import de.hpi.sam.warehouse.stock.Stockroom;
+import de.hpi.sam.warehouse.stock.StockroomID;
 
 
 
 
 public class CartSource extends RoomPointCartArea {
-	WarehouseRobot warehouseRobot = new WarehouseRobot(new RobotinoID("000.000.000.000"));
+	private WarehouseRobot warehouseRobot;
 	
-	public CartSource(CartArea cartArea) {
+	public CartSource(CartArea cartArea, StockroomID stock) {
 		super(cartArea);
 		carts = cartArea.getCartPositions();
 		this.location = (Position)carts.get(0);
-		this.room = warehouseRobot.getRoomFor(location);
-	}
+		//this.room = warehouseRobot.getRoomFor(location);
+		this.room = room; 
+	}	
 	
 	@Override
 	public Cart interact(Cart cart,  WarehouseRobot robot){
+		this.warehouseRobot = robot;
 		if (carts.size() < 1)
 			return null;
 		CartPosition cartPosition = carts.get(0);
